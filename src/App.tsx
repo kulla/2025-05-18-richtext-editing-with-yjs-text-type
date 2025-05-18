138ba6a77941ceaf46d145c8be99274aea8db385
Stephan Kulla <git.mail@kulla.me>
2025-05-18 11:42:12 +0200

    feat: Add render function for rich text

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import * as Y from 'yjs'
 import './App.css'
 import { useRef, useSyncExternalStore } from 'react'
 import { isEqual } from 'lodash'
+import { clsx } from 'clsx'
 
 const ydoc = new Y.Doc()
 const ytext = ydoc.getText('text')
 
 // Insert some initial text
-ytext.insert(0, 'Hello World!')
+ytext.insert(0, 'Hello ')
+ytext.insert(6, 'World!', { bold: true })
 
 export default function App() {
-  const previousText = useRef(null)
+  const previousText = useRef<RichText>(null)
   const text = useSyncExternalStore(
     (callback) => {
       ytext.observe(callback)
@@ -20,7 +22,9 @@ export default function App() {
       }
     },
     () => {
-      const text = ytext.toDelta()
+      // In a real application we need to some tyoe checking here (for example
+      // with io-ts)
+      const text = ytext.toDelta() as RichText
 
       if (isEqual(previousText.current, text)) {
         return previousText.current
@@ -33,10 +37,41 @@ export default function App() {
 
   return (
     <main className="prose p-10">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <h1>Richtext:</h1>
+      {text != null ? <RichText text={text} /> : <p>Loading...</p>}
       <h1 className="mt-5">Internal state</h1>
       <pre>{JSON.stringify(text, null, 2)}</pre>
     </main>
   )
+}
+
+function RichText({ text }: { text: RichText }) {
+  return (
+    <p id="richtext">
+      {text.map((item, index) => {
+        return (
+          <span
+            // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+            key={index}
+            className={clsx(
+              item.attributes?.bold ? 'font-bold' : null,
+              item.attributes?.italic ? 'italic' : null,
+            )}
+          >
+            {item.insert}
+          </span>
+        )
+      })}
+    </p>
+  )
+}
+
+type RichText = Array<InsertText>
+
+interface InsertText {
+  insert: string
+  attributes?: {
+    bold?: boolean
+    italic?: boolean
+  }
 }
